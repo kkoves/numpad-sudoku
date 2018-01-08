ff4ce6985933fa2be784f1f95275d51796635927
Krisztián Köves <kkoves3@gmail.com>
2018-01-08 01:00:06 -0500

    Added highlighting for each 3x3 square with number keys

--- sudoku.js
+++ sudoku.js
@@ -1,5 +1,21 @@
+var actionList = [0, 0, 0];
+var currAction = -1;
+
 $(document).ready(function() {
 	$(document).keypress(function(e) {
 		console.log(e.key);
+
+		if(e.key >= 0 && e.key <= 9) {
+			$(".highlighted").removeClass("highlighted");
+			highlight3By3(e.key);
+		}
 	});
 });
+
+function highlight3By3(squareNumber) {
+	$(".square" + squareNumber).addClass("highlighted");
+}
+
+function highlightOneSquare(squareNumber) {
+
+}
