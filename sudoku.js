3101bd3b83bd4b95f96462e23a1831434bc8b1cd
Krisztián Köves <kkoves3@gmail.com>
2018-01-08 23:37:43 -0500

    Finished basic grid entry mechanic by adding cell select and write

--- sudoku.js
+++ sudoku.js
@@ -1,21 +1,78 @@
-var actionList = [0, 0, 0];
+var actionSet = [0, 0, 0];
 var currAction = -1;
+
+var cellLookupTable = [null, 6, 7, 8, 3, 4, 5, 0, 1, 2];
 
 $(document).ready(function() {
 	$(document).keypress(function(e) {
-		console.log(e.key);
+		// 1-9 are used to select a block or cell, or to fill a cell
+		if(e.key >= 1 && e.key <= 9) {
+			currAction++;
+			clearHighlight();
 
-		if(e.key >= 0 && e.key <= 9) {
-			$(".highlighted").removeClass("highlighted");
-			highlight3By3(e.key);
+			if(currAction == 0) {
+				actionSet[0] = e.key;
+				highlightBlock(e.key);
+			}
+			else if(currAction == 1) {
+				actionSet[1] = e.key;
+				highlightCell(actionSet.slice(0, 2));
+			}
+			else if(currAction == 2) {
+				actionSet[2] = e.key;
+				writeValueToCell(actionSet.slice(0, 2), actionSet[2]);
+			}
+		}
+		// 0 will be used to clear the value of a cell on the grid, or to cancel an action set
+		else if(e.key === 0) {
+			currAction++;
+
+			if(currAction == 1)
+				clearActionSet();
+
+			if(currAction == 2)
+				writeValueToCell(actionSet.slice(0, 2), actionSet[2]);
+		}
+		// The period (.) key will be used to cancel an action set
+		else if(e.key == ".") {
+			clearActionSet();
 		}
 	});
 });
 
-function highlight3By3(squareNumber) {
-	$(".square" + squareNumber).addClass("highlighted");
+function highlightBlock(blockNumber) {
+	$(".square" + blockNumber).addClass("highlighted");
 }
 
-function highlightOneSquare(squareNumber) {
+function highlightCell(coordinates) {
+	var selector = ".square" + coordinates[0];
+	var block = $(selector);
+	var index = cellLookupTable[coordinates[1]];
 
+	var cell = block[index];
+
+	$(cell).addClass("highlighted");
+}
+
+function clearHighlight() {
+	$(".highlighted").removeClass("highlighted");
+}
+
+function writeValueToCell(coordinates, value) {
+	var selector = ".square" + coordinates[0];
+	var block = $(selector);
+	var index = cellLookupTable[coordinates[1]];
+
+	var cell = block[index];
+
+	$(cell).text(value);
+
+	clearActionSet();
+}
+
+function clearActionSet() {
+	clearHighlight();
+
+	actionSet = new Array(3);
+	currAction = -1;
 }
